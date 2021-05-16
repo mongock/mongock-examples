a1f4144ea9531b93f37dd7a99d472a41033e18e0
Oliver Santana <oliver.sm@gmail.com>
2021-05-16 16:04:19 +0100

    Added standalone-springdata-advanced example. Removed ZonedDateTime converters from related basic example.

---
+++ mongoDB/standalone-springdata-advanced/src/main/java/com/github/cloudyrock/mongock/examples/changelogs/secondarydb/PopulateDataSecondaryDbChangeLog.java
@@ -0,0 +1,35 @@
+package com.github.cloudyrock.mongock.examples.changelogs.secondarydb;
+
+import com.github.cloudyrock.mongock.ChangeLog;
+import com.github.cloudyrock.mongock.ChangeSet;
+import com.github.cloudyrock.mongock.NonLockGuarded;
+import com.github.cloudyrock.mongock.PreMigration;
+import com.github.cloudyrock.mongock.examples.product.Product;
+
+import javax.inject.Named;
+import java.util.stream.IntStream;
+
+import static com.github.cloudyrock.mongock.examples.StandaloneSpringDataAdvancedApp.PRODUCTS_COLLECTION_NAME;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@PreMigration
+@ChangeLog(order = "2")
+public class PopulateDataSecondaryDbChangeLog {
+  
+  public final static int INITIAL_PRODUCTS = 10;
+
+  @ChangeSet(id = "populate-data-secondarydb", order = "001", author = "mongock")
+  public void populateDataSecondaryDb(@Named("secondaryMongoTemplate") @NonLockGuarded MongoTemplate secondaryTemplate) {
+    
+    // NOTE: We use this PreMigration ChangeLog to populate data in secondaryDb for examples to work.
+    // But we should only use secondary databases for read, because they won't be managed by Mongock
+    // lock and transactions.
+    IntStream.range(0, INITIAL_PRODUCTS)
+             .mapToObj(PopulateDataSecondaryDbChangeLog::getProduct)
+             .forEach(product -> secondaryTemplate.insert(product, PRODUCTS_COLLECTION_NAME));
+  }
+  
+  private static Product getProduct(int i) {
+    return new Product("product-" + i, 5 * i, 100.25 * i);
+  }
+}

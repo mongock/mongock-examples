4f7a73664b57eddb6535b2b51b2dd20535fd80a6
Oliver Santana <oliver.sm@gmail.com>
2021-05-27 19:50:05 +0100

    Refactor event listeners after core changes.

--- mongoDB/standalone-springdata-advanced/src/main/java/com/github/cloudyrock/mongock/examples/events/MongockEventListener.java
+++ mongoDB/standalone-springdata-advanced/src/main/java/com/github/cloudyrock/mongock/examples/events/MongockEventListener.java
@@ -1,19 +1,20 @@
 package com.github.cloudyrock.mongock.examples.events;
 
-import com.github.cloudyrock.mongock.runner.core.event.result.MigrationResult;
-
+import com.github.cloudyrock.mongock.runner.core.event.MigrationFailureEvent;
+import com.github.cloudyrock.mongock.runner.core.event.MigrationStartedEvent;
+import com.github.cloudyrock.mongock.runner.core.event.MigrationSuccessEvent;
 
 public class MongockEventListener {
 
-  public static void onStart() {
+  public static void onStart(MigrationStartedEvent event) {
     System.out.println("[EVENT LISTENER] - Mongock STARTED successfully");
   }
 
-  public static void onSuccess(MigrationResult result) {
+  public static void onSuccess(MigrationSuccessEvent event) {
     System.out.println("[EVENT LISTENER] - Mongock finished successfully");
   }
 
-  public static void onFail(Exception exception) {
+  public static void onFail(MigrationFailureEvent event) {
     System.out.println("[EVENT LISTENER] - Mongock finished with failures");
   }
 }

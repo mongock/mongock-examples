3c69f3a45669f8a1e5f7febface8a605bcf5f744
dieppa <aperezdieppa@gmail.com>
2021-07-14 17:15:30 +0100

    sptingboot mysql annotation basic

--- mongoDB/springboot-mysql-annotation-basic/src/main/java/com/github/cloudyrock/mongock/examples/SpringBootMysqlAnnotationBasicApp.java
+++ mongoDB/springboot-mysql-annotation-basic/src/main/java/com/github/cloudyrock/mongock/examples/SpringBootMysqlAnnotationBasicApp.java
@@ -1,19 +1,11 @@
 package com.github.cloudyrock.mongock.examples;
 
 
-import com.github.cloudyrock.mongock.driver.api.driver.ConnectionDriver;
-import com.github.cloudyrock.mongock.driver.api.entry.ChangeEntry;
-import com.github.cloudyrock.mongock.driver.sql.spring.jpa.driver.SpringJdbcSqlDriver;
 import com.github.cloudyrock.mongock.examples.repository.UserRepository;
-import com.github.cloudyrock.springboot.MongockSpringboot;
-import com.github.cloudyrock.springboot.base.MongockInitializingBeanRunner;
+import com.github.cloudyrock.mongock.jdbc.springboot.EnableMongockJdbc;
 import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
@@ -22,6 +14,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import javax.sql.DataSource;
 
 @SpringBootApplication
+@EnableMongockJdbc
 @EnableJdbcRepositories(basePackageClasses = UserRepository.class)
 public class SpringBootMysqlAnnotationBasicApp {
 
@@ -47,29 +40,29 @@ public class SpringBootMysqlAnnotationBasicApp {
         return new JdbcTemplate(dataSource);
     }
 
-    @Bean
-    public ConnectionDriver<ChangeEntry> connectionDriver(JdbcTemplate applicationJdbcTemplate) {
-        SpringJdbcSqlDriver driver  = SpringJdbcSqlDriver.mysqlWithDefaultLock(applicationJdbcTemplate);
-        return driver;
-    }
-
-    @Bean
-    public MongockInitializingBeanRunner mongockInitializingBeanRunner(
-            ApplicationContext springContext,
-            ConnectionDriver<ChangeEntry> driver,
-            ApplicationEventPublisher eventPublisher) {
-
-
-        // Runner
-        return MongockSpringboot.builder()
-                .setDriver(driver)
-                .addChangeLogsScanPackage("com.github.cloudyrock.mongock.examples.changelogs")
-                .setSpringContext(springContext)
-                .setEventPublisher(eventPublisher)
-                .setTrackIgnored(true)
-                .setTransactionEnabled(true)
-                .buildInitializingBeanRunner();
-    }
+//    @Bean
+//    public ConnectionDriver<ChangeEntry> connectionDriver(JdbcTemplate applicationJdbcTemplate) {
+//        SpringJdbcDriver driver  = SpringJdbcDriver.mysqlWithDefaultLock(applicationJdbcTemplate);
+//        return driver;
+//    }
+//
+//    @Bean
+//    public MongockInitializingBeanRunner mongockInitializingBeanRunner(
+//            ApplicationContext springContext,
+//            ConnectionDriver<ChangeEntry> driver,
+//            ApplicationEventPublisher eventPublisher) {
+//
+//
+//        // Runner
+//        return MongockSpringboot.builder()
+//                .setDriver(driver)
+//                .addChangeLogsScanPackage("com.github.cloudyrock.mongock.examples.changelogs")
+//                .setSpringContext(springContext)
+//                .setEventPublisher(eventPublisher)
+//                .setTrackIgnored(true)
+//                .setTransactionEnabled(true)
+//                .buildInitializingBeanRunner();
+//    }
 
 
 
--- mongoDB/springboot-mysql-annotation-basic/src/main/java/com/github/cloudyrock/mongock/examples/changelogs/ClientInitializerChangeLog.java
+++ mongoDB/springboot-mysql-annotation-basic/src/main/java/com/github/cloudyrock/mongock/examples/changelogs/ClientInitializerChangeLog.java
@@ -2,39 +2,26 @@ package com.github.cloudyrock.mongock.examples.changelogs;
 
 import com.github.cloudyrock.mongock.ChangeLog;
 import com.github.cloudyrock.mongock.ChangeSet;
-import com.github.cloudyrock.mongock.examples.domain.User;
-import com.github.cloudyrock.mongock.examples.repository.UserRepository;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.time.LocalDateTime;
-import java.util.Iterator;
-import java.util.List;
+import com.github.cloudyrock.mongock.NonLockGuarded;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @ChangeLog(order = "1")
 public class ClientInitializerChangeLog {
 
-    public final static int INITIAL_CLIENTS = 10;
-
-    @ChangeSet(id = "data-initializer", order = "002", author = "mongock")
-    public void dataInitializer(UserRepository repository) throws SQLException {
-//        Iterable<User> users = repository.findAll();
-        System.out.println(repository.toString());
+    @ChangeSet(id = "user-create-table", order = "001", author = "mongock")
+    public void dataInitializer(@NonLockGuarded JdbcTemplate template)  {
+        template.execute("" +
+                "CREATE TABLE user (" +
+                "    id int primary key auto_increment," +
+                "    name varchar(255)," +
+                "    email varchar(255)" +
+                ")");
     }
 
-//    @ChangeSet(id = "data-initializer-2", order = "003", author = "mongock")
-//    public void dataInitializer2(Connection connection) throws SQLException {
-//        try (Statement statement = connection.createStatement()) {
-//            statement.executeUpdate(
-//                    String.format("INSERT INTO table_example(name) values('basic-example-2-%s');", LocalDateTime.now())
-//            );
-//        }
-//    }
-//
-//    @ChangeSet(id = "data-initializer-3", order = "003", author = "mongock")
-//    public void dataInitializer23(Connection connection) throws SQLException {
-//        if(true) throw new RuntimeException("Expected exception");
-//    }
+    @ChangeSet(id = "data-initializer-1", order = "002", author = "mongock")
+    public void dataInitializer1(@NonLockGuarded JdbcTemplate template) {
+        template.update("INSERT INTO user(name, email) values('name1', 'email1')");
+    }
+
 
 }

e7b47eefb93bc6ec8d4eb35f931281625a8e0da0
dieppa <aperezdieppa@gmail.com>
2021-09-03 18:04:41 +0100

    wip: springboot-springdata-builder-advanced

--- mongoDB/springboot-springdata-builder-advanced/src/main/java/com/github/cloudyrock/mongock/examples/changelogs/secondarydb/SecondaryDbChangeLog.java
+++ mongoDB/springboot-springdata-builder-advanced/src/main/java/com/github/cloudyrock/mongock/examples/changelogs/secondarydb/SecondaryDbChangeLog.java
@@ -2,11 +2,11 @@ package com.github.cloudyrock.mongock.examples.changelogs.secondarydb;
 
 import com.github.cloudyrock.mongock.ChangeLog;
 import com.github.cloudyrock.mongock.ChangeSet;
-import com.github.cloudyrock.mongock.NonLockGuarded;
 import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.impl.MongockTemplate;
 import com.github.cloudyrock.mongock.examples.product.Product;
 
 import com.mongodb.client.MongoDatabase;
+import io.changock.migration.api.annotations.NonLockGuarded;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import javax.inject.Named;

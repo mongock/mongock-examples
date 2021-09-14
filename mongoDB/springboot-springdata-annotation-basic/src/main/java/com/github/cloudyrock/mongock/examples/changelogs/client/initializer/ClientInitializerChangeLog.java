15149d96a77a15a4a2c0340bb2bf795aa851bdb2
dieppa <aperezdieppa@gmail.com>
2021-09-14 22:53:53 +0100

    version

--- mongoDB/springboot-springdata-annotation-basic/src/main/java/com/github/cloudyrock/mongock/examples/changelogs/client/initializer/ClientInitializerChangeLog.java
+++ mongoDB/springboot-springdata-annotation-basic/src/main/java/com/github/cloudyrock/mongock/examples/changelogs/client/initializer/ClientInitializerChangeLog.java
@@ -4,23 +4,62 @@ import com.github.cloudyrock.mongock.ChangeLog;
 import com.github.cloudyrock.mongock.ChangeSet;
 import com.github.cloudyrock.mongock.examples.client.Client;
 import com.github.cloudyrock.mongock.examples.client.ClientRepository;
+import com.mongodb.client.MongoDatabase;
 
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-@ChangeLog(order = "1")
-public class ClientInitializerChangeLog {
+import static com.github.cloudyrock.mongock.examples.SpringBootSpringDataAnnotationBasicApp.CLIENTS_COLLECTION_NAME;
+import static com.mongodb.client.model.Filters.in;
+
+public class ClientInitializerChangeLog implements io.mongock.api.ChangeLog {
+
+  public static final String ID = "client-initializer";
 
   public final static int INITIAL_CLIENTS = 10;
 
-  @ChangeSet(id = "data-initializer-with-repository", order = "001", author = "mongock")
-  public void dataInitializer(ClientRepository clientRepository) {
-    
+  private final ClientRepository clientRepository;
+
+  public ClientInitializerChangeLog(ClientRepository clientRepository) {
+    this.clientRepository = clientRepository;
+  }
+
+  @Override
+  public String geId() {
+    return ID;
+  }
+
+  @Override
+  public String getOrder() {
+    return "1";
+  }
+
+  @Override
+  public String getAuthor() {
+    return "mongock";
+  }
+
+  @Override
+  public void before() {
+
+  }
+
+  @Override
+  public void rollbackBefore() {
+  }
+
+  @Override
+  public void changeSet() {
     clientRepository.saveAll(
             IntStream.range(0, INITIAL_CLIENTS)
                     .mapToObj(ClientInitializerChangeLog::getClient)
                     .collect(Collectors.toList())
     );
+  }
+
+  @Override
+  public void rollback() {
+    clientRepository.deleteAll();
   }
 
   private static Client getClient(int i) {
--- mongoDB/springboot-springdata-annotation-basic/src/main/java/com/github/cloudyrock/mongock/examples/changelogs/client/updater/ClientUpdaterChangeLog.java
+++ mongoDB/springboot-springdata-annotation-basic/src/main/java/com/github/cloudyrock/mongock/examples/changelogs/client/updater/ClientUpdaterChangeLog.java
@@ -4,19 +4,62 @@ import com.github.cloudyrock.mongock.ChangeLog;
 import com.github.cloudyrock.mongock.ChangeSet;
 import com.github.cloudyrock.mongock.examples.client.Client;
 import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.impl.MongockTemplate;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import io.mongock.api.BasicChangeLog;
+import org.bson.Document;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import static com.github.cloudyrock.mongock.examples.SpringBootSpringDataAnnotationBasicApp.CLIENTS_COLLECTION_NAME;
 
-@ChangeLog(order = "2")
-public class ClientUpdaterChangeLog {
+public class ClientUpdaterChangeLog  implements BasicChangeLog {
 
-  @ChangeSet(id = "data-updater-with-mongockTemplate", order = "001", author = "mongock")
-  public void dataUpdater(MongockTemplate template) {
+  private final MongoTemplate mongoTemplate;
 
-    template.findAll(Client.class, CLIENTS_COLLECTION_NAME)
+  public ClientUpdaterChangeLog(MongoTemplate mongoTemplate) {
+    this.mongoTemplate = mongoTemplate;
+  }
+
+  @Override
+  public String geId() {
+    return "client-updater";
+  }
+
+  @Override
+  public String getOrder() {
+    return "2";
+  }
+
+  @Override
+  public String getAuthor() {
+    return "mongock";
+  }
+
+  @Override
+  public boolean isRunAlways() {
+    return true;
+  }
+
+  @Override
+  public void changeSet() {
+
+    mongoTemplate.findAll(Client.class, CLIENTS_COLLECTION_NAME)
             .stream()
             .map(client -> client.setName(client.getName() + "_updated"))
-            .forEach(client -> template.save(client, CLIENTS_COLLECTION_NAME));
+            .forEach(client -> mongoTemplate.save(client, CLIENTS_COLLECTION_NAME));
+  }
+
+  @Override
+  public void rollback() {
+    mongoTemplate.findAll(Client.class, CLIENTS_COLLECTION_NAME)
+            .stream()
+            .map(client -> client.setName(client.getName().substring(0, client.getName().length() - "_updated".length())))
+            .forEach(client -> mongoTemplate.save(client, CLIENTS_COLLECTION_NAME));
 
   }
+
 }

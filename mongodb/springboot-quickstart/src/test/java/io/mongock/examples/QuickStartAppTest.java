0e12584ea690b375bba73a8fba0bf4c3443cd1fe
antoniop perez dieppa <aperezdieppa@gmail.com>
2022-04-29 10:29:04 +0100

    using mongock testing framework

--- mongodb/springboot-quickstart/src/test/java/io/mongock/examples/QuickStartAppTest.java
+++ mongodb/springboot-quickstart/src/test/java/io/mongock/examples/QuickStartAppTest.java
@@ -1,28 +1,63 @@
 package io.mongock.examples;
 
+import com.mongodb.client.MongoDatabase;
+import io.mongock.api.exception.MongockException;
 import io.mongock.examples.changelogs.ClientInitializerChangeLog;
 import io.mongock.examples.client.ClientRepository;
+import io.mongock.examples.config.MongoInitializer;
+import io.mongock.runner.core.builder.RunnerBuilder;
+import io.mongock.test.springboot.MongockSpringbootIntegrationTestBase;
+import io.mongock.test.springboot.junit5.MongockSpringbootJUnit5IntegrationTestBase;
+import org.bson.Document;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
+import static io.mongock.examples.QuickStartApp.CLIENTS_COLLECTION_NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Quick example of testing Mongock migration with Springboot
  */
-@ExtendWith(SpringExtension.class)
+@ContextConfiguration(initializers = MongoInitializer.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class QuickStartAppTest {
+class QuickStartAppTest extends MongockSpringbootJUnit5IntegrationTestBase {
 
     @Autowired
     private ClientRepository clientRepository;
 
+
+    private MongoDatabase mongoDatabase;
+
+    @Autowired
+    private void initDatabase(MongoDatabaseFactory mongoDatabaseFactory) {
+        mongoDatabase = mongoDatabaseFactory.getMongoDatabase();
+    }
+
+    @AfterEach
+    void afterEach() {
+        mongoDatabase.getCollection("mongockChangeLog").deleteMany(new Document());
+        mongoDatabase.getCollection("mongockLock").deleteMany(new Document());
+        mongoDatabase.getCollection(CLIENTS_COLLECTION_NAME).drop();
+    }
+
     @Test
-    void shouldStoreClientsInRepository() {
+    void shouldRunSuccessfullyWhenCollectionIsNotCreatedYet() {
+        executeMongock();
         assertEquals(ClientInitializerChangeLog.INITIAL_CLIENTS, clientRepository.findAll().size());
+    }
+    
+    @Test
+    void shouldFailWhenCollectionIsAlreadyCreated() {
+        mongoDatabase.createCollection(CLIENTS_COLLECTION_NAME);
+        MongockException ex = Assertions.assertThrows(MongockException.class, this::executeMongock);
+        Assertions.assertTrue(ex.getMessage().contains("Collection already exists"));
 
     }
 
